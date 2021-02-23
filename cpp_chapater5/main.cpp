cba0302ee2b085374fce655931aadb6bcb29ae61
irokanade <michaeljs.leong@gmail.com>
2021-02-23 22:44:21 +0800

    Initial Commit

---
+++ cpp_chapater5/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_chapater5
+//
+//  Created by Michael Leong on 2021/2/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
